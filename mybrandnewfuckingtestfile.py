eee9c0668c6a85abafce29cc16a5d6c5ee21ac96
tuod <45567514+tuod@users.noreply.github.com>
2019-03-03 01:50:06 +0400

    Update mybrandnewfuckingtestfile.py

--- mybrandnewfuckingtestfile.py
+++ mybrandnewfuckingtestfile.py
@@ -10,11 +10,19 @@ instagram reddit flickr meetup classmates microsoft apple
 linkedin xing renren disqus snapchat twoo whatsapp""".split()
 
 
-def convert_bytes(num):
+def bytes_to_human(num):
     for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
         if num < 1024.0:
-            return "%3.1f %s" % (num, x)
+            return f'{num:3.1f} {x}'
         num /= 1024.0
+
+
+def seconds_to_human(sec):
+    for x, y in [(60, 'seconds'), (60, 'minutes'), (24, 'hours'),
+                 (7, 'days'), (0, 'weeks')]:
+        if sec < x or not x:
+            return f'{sec:.1f} {y}'
+        sec /= x
 
 
 def running_time():
@@ -28,9 +36,9 @@ async def fetch(url, ioloop, executor):
         data = http_rq.read
         code = http_rq.getcode
         # sleep(3)
-        print(f'{url} - code {code()} - {convert_bytes(len(data()))} - {running_time():{2}.{2}} sec')
+        print(f'{url} - code {code()} - {bytes_to_human(len(data()))} - {seconds_to_human(running_time())}')
     except error.HTTPError as e:
-        print(f'{url} - Exception {e} - {running_time():{12}.{2}} sec')
+        print(f'{url} - Exception {e} - {running_time():.2f} sec')
 
 
 template = 'http://www.{}.com'
